3516266f458875f4fba83ac0ee718b6a87d2c3ef
Wassef Hassine <hassine.wassef@outlook.com>
2022-01-29 18:33:30 -0800

    creating stateful todo page

---
+++ lib/pages/todo_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({Key? key}) : super(key: key);
+
+  @override
+  _TodoPageState createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
