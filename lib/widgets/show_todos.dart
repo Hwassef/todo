e0daf0d1b3262a111aa09c24003c2a61fc6028a2
Wassef Hassine <hassine.wassef@outlook.com>
2022-01-30 07:40:06 -0800

    Finishing Up

--- lib/widgets/show_todos.dart
+++ lib/widgets/show_todos.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/providers/filtred_todos.dart';
+import 'package:todo_app/widgets/show_background.dart';
 
 class ShowTodos extends StatelessWidget {
   const ShowTodos({Key? key}) : super(key: key);
@@ -18,8 +19,8 @@ class ShowTodos extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return Dismissible(
           key: ValueKey(todos[index].id),
-          background: showBackground(),
-          secondaryBackground: showBackground(),
+          background: ShowBackground(0),
+          secondaryBackground: ShowBackground(1),
           child: Text(
             todos[index].desc,
             style: TextStyle(fontSize: 18.0),
