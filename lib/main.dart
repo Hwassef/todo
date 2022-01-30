3fe0565f175eb8011382cbedfaa3f19c225d8459
Wassef Hassine <hassine.wassef@outlook.com>
2022-01-30 05:11:57 -0800

    injecting providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/pages/todo_page.dart';
+import 'package:todo_app/providers/active_todo_count.dart';
+import 'package:todo_app/providers/filtred_todos.dart';
+import 'package:todo_app/providers/todo_filter.dart';
+import 'package:todo_app/providers/todo_list.dart';
+import 'package:todo_app/providers/todo_search.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +15,43 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODO',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (context) => TodoFilter(),
+        ),
+        ChangeNotifierProvider<TodoSearch>(
+          create: (context) => TodoSearch(),
+        ),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(),
+          update: (
+            BuildContext context,
+            TodoList todoList,
+            ActiveTodoCount? activeTodoCount,
+          ) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList,
+            FiltredTodos>(
+          create: (context) => FiltredTodos(),
+          update: (
+            BuildContext context,
+            TodoFilter todoFilter,
+            TodoSearch todoSearch,
+            TodoList todoList,
+            FiltredTodos? filtredTodos,
+          ) =>
+              filtredTodos!..update(todoFilter, todoSearch, todoList),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TODO',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Todo(),
       ),
-      home: const Todo(),
     );
   }
 }
