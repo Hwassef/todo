37634c4140feab543760f0d1ab2e00c5519cd29c
Wassef Hassine <hassine.wassef@outlook.com>
2022-01-30 06:36:59 -0800

    Finishin up TodoHeader

--- lib/widgets/todo_header.dart
+++ lib/widgets/todo_header.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/src/provider.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/providers/active_todo_count.dart';
 
 class TodoHeader extends StatelessWidget {
@@ -7,6 +7,7 @@ class TodoHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         const Text(
           'TODO',
