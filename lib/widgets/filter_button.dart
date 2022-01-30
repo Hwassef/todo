e44883976b17f4f8af382a1b68703cc682aa0139
Wassef Hassine <hassine.wassef@outlook.com>
2022-01-30 07:07:07 -0800

    Creating 'FilterButton' Widget

---
+++ lib/widgets/filter_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/models/todo_model.dart';
+
+@override
+Widget build(BuildContext context, Filter filter) {
+  return TextButton(
+    onPressed: () {},
+    child: Text(
+      filter == Filter.All
+          ? 'All'
+          : filter == Filter.Active
+              ? 'Active'
+              : 'Completed',
+      style: const TextStyle(
+        fontSize: 18.0,
+        color: TextColor(context, filter),
+      ),
+    ),
+  );
+}
