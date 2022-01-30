4d09dbf2dbba5c57781d454c904b3fa93c2a59bd
Wassef Hassine <hassine.wassef@outlook.com>
2022-01-30 07:03:16 -0800

    Creating 'SearchAndFilter' Widget

---
+++ lib/widgets/search_and_filter_todo.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:todo_app/models/todo_model.dart';
+import 'package:todo_app/providers/todo_search.dart';
+
+class SearchAndFilterTodo extends StatelessWidget {
+  const SearchAndFilterTodo({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          decoration: const InputDecoration(
+            labelText: 'Search Todos',
+            border: InputBorder.none,
+            filled: true,
+            prefixIcon: Icon(Icons.search),
+          ),
+          onChanged: (String? newSearchTerm) {
+            if (newSearchTerm != null) {
+              context.read<TodoSearch>().setSearchTerm(newSearchTerm);
+            }
+          },
+        ),
+        SizedBox(height: 20.0),
+        Row(
+          children: [
+            filterButton(context, Filter.All),
+            filterButton(context, Filter.Active),
+            filterButton(context, Filter.Completed),
+          ],
+        )
+      ],
+    );
+  }
+}
